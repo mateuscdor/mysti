c693a384588c12b584cffc56d05dc54834ca9c1d
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-03-25 15:36:48 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/game-tebakkata_ans.js
+++ plugins/game-tebakkata_ans.js
@@ -2,7 +2,7 @@ import similarity from 'similarity'
 const threshold = 0.72
 export async function before(m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*teka/i.test(m.quoted.text))
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Escriba.*marque/i.test(m.quoted.text))
         return !0
     this.tebakkata = this.tebakkata ? this.tebakkata : {}
     if (!(id in this.tebakkata))
@@ -12,7 +12,7 @@ export async function before(m) {
         // m.reply(JSON.stringify(json, null, '\t'))
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.tebakkata[id][2]
-            conn.sendButton(m.chat, `*benar mendapatkan*\n+${this.tebakkata[id][2]} XP`, wm, ['Tebakkata', '/tebakkata'], m)
+            conn.sendButton(m.chat, `*hazlo bien*\n+${this.tebakkata[id][2]} XP`, wm, ['Tebakkata', '/tebakkata'], m)
             clearTimeout(this.tebakkata[id][3])
             delete this.tebakkata[id]
         } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold)
