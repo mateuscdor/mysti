1751f1df7250ea73a071f11cb6de8ab99eb5f3de
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-03-24 20:20:05 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/menu.js
+++ plugins/menu.js
@@ -149,7 +149,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     const pp = await conn.profilePictureUrl(conn.user.jid, 'image').catch(_ => './src/avatar_contact.png')
-    conn.sendHydrated(m.chat, text.trim(), author, pp, 'https://github.com/BrunoSobrino/TheMystic-Bot-MD', '𝙶𝙸𝚃𝙷𝚄𝙱', owner[0][0], '𝙾𝚆𝙽𝙴𝚁', [
+    conn.sendHydrated(m.chat, text.trim(), author, pp, 'https://chat.whatsapp.com/E9qJfvlLjENKAxnhII9rao', 'GRUPO', owner[0][0], '𝙾𝚆𝙽𝙴𝚁', [
       ['𝙳𝙾𝙽𝙰𝚁', '/donasi'],
       ['𝚂𝙿𝙴𝙴𝙳', '/ping'],
       ['𝙾𝚆𝙽𝙴𝚁', '/owner']
