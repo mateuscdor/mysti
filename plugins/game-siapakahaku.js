8596bb0d56499e62f084e73303b2c3bc55c31178
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-03-25 15:28:04 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/game-siapakahaku.js
+++ plugins/game-siapakahaku.js
@@ -6,21 +6,21 @@ let handler = async (m, { conn, usedPrefix }) => {
     conn.siapakahaku = conn.siapakahaku ? conn.siapakahaku : {}
     let id = m.chat
     if (id in conn.siapakahaku) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.siapakahaku[id][0])
+        conn.reply(m.chat, 'Todavía hay preguntas sin respuesta en este chat', conn.siapakahaku[id][0])
         throw false
     }
     const json = await siapakahaku()
     let caption = `
-Siapakah aku? ${json.soal}
-Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}who untuk bantuan
+¿Quién soy? ${json.soal}
+Se acaba el tiempo *${(timeout / 1000).toFixed(2)} detik*
+Escrive ${usedPrefix}who untuk bantuan
 Bonus: ${poin} XP
 `.trim()
     conn.siapakahaku[id] = [
         await conn.sendButton(m.chat, caption, author, ['hint', `/who`], m),
         json, poin,
         setTimeout(() => {
-            if (conn.siapakahaku[id]) conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, author, ['siapahaku', '/siapakahaku'], conn.siapakahaku[id][0])
+            if (conn.siapakahaku[id]) conn.sendButton(m.chat, `el tiempo se ha acabado!\nLa respuesta es *${json.jawaban}*`, author, ['siapahaku', '/siapakahaku'], conn.siapakahaku[id][0])
             delete conn.siapakahaku[id]
         }, timeout)
     ]
