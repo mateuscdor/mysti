7d4318958420b1076e5b0ca855ca26cbfaee0a7e
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-03-25 14:27:19 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-tiktokaudio.js
+++ plugins/downloader-tiktokaudio.js
@@ -1,7 +1,7 @@
 import hxz from 'hxz-api'
 
 let handler = async(m,{text, conn, usedPrefix, command}) => {
-if (!text) throw `Use example ${usedPrefix}${command}\nhttps://vt.tiktok.com/ZGJBtcsDq/`
+if (!text) throw `Ejemplo de uso ${usedPrefix}${command}\nhttps://vt.tiktok.com/ZGJBtcsDq/`
 let p = await  hxz.ttdownloader(text)
 const { nowm, wm, audio } = p
 conn.sendFile(m.chat, audio, null, null, m)
