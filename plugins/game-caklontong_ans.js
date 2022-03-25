242e84367a2d5c1e5435bafdf753741f94a575cd
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-03-25 15:08:56 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/game-caklontong_ans.js
+++ plugins/game-caklontong_ans.js
@@ -5,16 +5,16 @@ export async function before(m) {
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/Ketik.*calo/i.test(m.quoted.text) || /.*(calo|bantuan)/i.test(m.text))
         return !0
     this.caklontong = this.caklontong ? this.caklontong : {}
-    if (!(id in this.caklontong)) return m.reply('Soal itu telah berakhir')
+    if (!(id in this.caklontong)) return m.reply('El asunto ha terminado')
     if (m.quoted.id == this.caklontong[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.caklontong[id][1]))
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.caklontong[id][2]
-            await this.sendButton(m.chat, `*Benar!* +${this.caklontong[id][2]} XP\n${json.deskripsi}`, author, null, [['Cak Lontong', '.caklontong']], m)
+            await this.sendButton(m.chat, `*Derecha!* +${this.caklontong[id][2]} XP\n${json.deskripsi}`, author, null, [['Cak Lontong', '.caklontong']], m)
             clearTimeout(this.caklontong[id][3])
             delete this.caklontong[id]
         } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold)
-            m.reply(`*Dikit Lagi!*`)
+            m.reply(`*¡Un poco mas!*`)
         else {
             if (m.text != '/calo') return
             m.reply(`*Salah!*`)
