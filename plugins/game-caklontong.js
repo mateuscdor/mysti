c94e32173fd69e7758a7cb579f7c0c01d2c83846
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-03-25 15:05:33 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/game-caklontong.js
+++ plugins/game-caklontong.js
@@ -5,19 +5,19 @@ let poin = 4999
 let handler = async (m, { conn, usedPrefix }) => {
     conn.caklontong = conn.caklontong ? conn.caklontong : {}
     let id = m.chat
-    if (id in conn.caklontong) return conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.caklontong[id][0])
+    if (id in conn.caklontong) return conn.reply(m.chat, 'Todavía hay preguntas sin responder en este chat', conn.caklontong[id][0])
     let json = await caklontong()
     let caption = `
 ${json.soal}
-Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}calo untuk bantuan
+Tiempo de espera *${(timeout / 1000).toFixed(2)} detik*
+Escrive ${usedPrefix}calo untuk bantuan
 Bonus: ${poin} XP
 `.trim()
     conn.caklontong[id] = [
         await conn.sendButton(m.chat, caption, author, null, [['Bantuan', `/calo`]], m),
         json, poin,
         setTimeout(async () => {
-            if (conn.caklontong[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*\n${json.deskripsi}`, author, null, [['Cak Lontong', `/caklontong`]], conn.caklontong[id][0])
+            if (conn.caklontong[id]) await conn.sendButton(m.chat, `el tiempo se ha acabado!\nLa respuesta es *${json.jawaban}*\n${json.deskripsi}`, author, null, [['Cak Lontong', `/caklontong`]], conn.caklontong[id][0])
             delete conn.caklontong[id]
         }, timeout)
     ]
