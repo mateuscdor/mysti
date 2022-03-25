d72d4b4e0d2f0a49a5a50ec5f36ddd1ad3d06133
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-03-25 14:55:45 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/fun-kapankah.js
+++ plugins/fun-kapankah.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => conn.reply(m.chat, `
 *Pertanyaan:* ${m.text}
-*Jawaban:* ${(10).getRandom()} ${['detik', 'menit', 'jam', 'hari', 'minggu', 'bulan', 'tahun', 'dekade', 'abad'].getRandom()} lagi ...
+*Jawaban:* ${(10).getRandom()} ${['segundo', 'minuto', 'hora', 'día', 'semana', 'mes', 'año', 'década', 'siglo'].getRandom()} otra vez ...
   `.trim(), m, m.mentionedJid ? {
     mentions: m.mentionedJid
 } : {})
