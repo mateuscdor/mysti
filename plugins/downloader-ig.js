ebfeb9d9ce97504c3268212a8674487de587a0eb
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-03-25 14:24:10 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,6 +1,6 @@
 import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
+    if (!args[0]) throw `Ejemplo de uso ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
     const results = await instagramdl(args[0])
         .catch(async _ => await instagramdlv2(args[0]))
         .catch(async _ => await instagramdlv3(args[0]))
