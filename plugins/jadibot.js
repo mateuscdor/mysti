a358c6dde4572eada497de394c0ee9022cafd1bf
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-03-29 11:56:02 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

---
+++ plugins/jadibot.js
@@ -0,0 +1,7 @@
+let handler = (t) => {
+t.reply('*Este bot hace subbots, pero se le puede crear uno para usted solo hable a este numero (wa.me/51977783315)*')
+}
+handler.command = ['serbot']
+handler.help = ['sc']
+handler.tags = ['general']
+export default handler
