1569cff2a1db99a2edd97c90a1ac6c6818ed114a
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-11 08:42:48 -0500

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -2,10 +2,10 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 
 let handler = async (m, { conn, text, isOwner }) => {
     let [_, code, expired] = text.match(linkRegex) || []
-    if (!code) throw 'Enlace invalido'
+    if (!code) throw 'Enlace invalido o sin enlace'
     let res = await conn.groupAcceptInvite(code)
     expired = Math.floor(Math.min(999, Math.max(1, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 1)))
-    m.reply(`Se unió al grupo con éxito ${res}${expired ? ` Durante ${expired} día` : ''}`)
+    m.reply(`Se unió al grupo con éxito ${res}${expired ? ` Durante ${expired} día, recuerde no saturar al bot` : ''}`)
     let chats = global.db.data.chats[res]
     if (!chats) chats = global.db.data.chats[res] = {}
     if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
@@ -14,7 +14,7 @@ handler.help = ['join <chat.whatsapp.com>']
 handler.tags = ['main']
 
 handler.command = /^join$/i
-handler.premium = true
+handler.premium = false
 
 export default handler
 
