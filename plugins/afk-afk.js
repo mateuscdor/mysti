2f3e8d90687bbfa06c5323540c533643c2f992ef
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-11 16:01:47 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -2,8 +2,7 @@ let handler = async (m, { text }) => {
     let user = global.db.data.users[m.sender]
     user.afk = + new Date
     user.afkReason = text
-    m.reply(`
-  ${conn.getName(m.sender)} ahora estas en AFK${text ? ': ' + text : ''}
+    m.reply(`*[ ⚠️ ] El usuario ${conn.getName(m.sender)} estará inactivo (AFK) no lo etiqueten*\n\n*🔰 Motivo de la inactividad${text ? ': ' + text : ''}*
   `)
 }
 handler.help = ['afk [alasan]']
