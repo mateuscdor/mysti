29844faa5a7b8a0fd3a00af996123bb09bd454c8
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-12 14:10:39 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/tourl.js
+++ plugins/tourl.js
@@ -4,13 +4,13 @@ import uploadImage from '../lib/uploadImage.js'
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'Tidak ada media yang ditemukan'
+  if (!mime) throw 'No se encontraron medios'
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
   m.reply(`${link}
 ${media.length} Byte(s)
-${isTele ? '(Tidak Ada Tanggal Kedaluwarsa)' : '(Tidak diketahui)'}`)
+${isTele ? '(Sin fecha de vencimiento)' : '(No conocida)'}`)
 }
 handler.help = ['tourl <reply image>']
 handler.tags = ['sticker']
