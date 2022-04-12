e7d7a717c82b85668cf2e21d3e80748990b9a783
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-12 08:08:07 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/anime-waifu.js
+++ plugins/anime-waifu.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix }) => {
-    let res = await fetch('https://api.waifu.pics/sfw/waifu')
+    let res = await fetch('https://api-alc.herokuapp.com/api/anime/waifu?&apikey=ConfuMods')
     if (!res.ok) throw await res.text()
     let json = await res.json()
     if (!json.url) throw 'Error!'
