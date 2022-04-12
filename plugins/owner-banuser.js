c5268248bc1b40827b9010b4be938cb796d1bf9e
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-12 10:10:12 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, text }) => {
-    if (!text) throw 'Siapa yang mau di banned?'
+    if (!text) throw '*A QUIEN QUIERE BANEAR?*'
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (!who) throw 'Tag salah satu lah'
+    if (!who) throw 'Etiqueta a alguien'
     let users = global.db.data.users
     users[who].banned = true
     conn.reply(m.chat, `berhasil banned`, m)
