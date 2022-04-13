29afdc937cb7e1ea7fc29705f97ec6cb58987c2c
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-13 06:02:57 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,6 +1,6 @@
 let handler = async (m) => {
     global.db.data.chats[m.chat].isBanned = false
-    m.reply('Done!')
+    m.reply('*✅ Listo.. chat desbaneado!*')
 }
 handler.help = ['unbanchat']
 handler.tags = ['group', 'owner']
