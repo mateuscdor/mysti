37744d3b0c7e517e6f74e577fb18bbe910f18029
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-15 11:28:12 -0500

    Update nulis=textpro.js

--- plugins/nulis=textpro.js
+++ plugins/nulis=textpro.js
@@ -2,9 +2,9 @@ import fetch from 'node-fetch'
 let split = '|'
 let handler = async (m, { conn, args: [effect], text: txt }) => {
   let { effects } = await (await (fetch(global.API('xteam', '/textpro')))).json()
-  if (!effect) throw '*List Effect*\n\nEXAMPLE:\n*.textpro 1917 zabot|zabot*\n\n' + effects.sort((a, b) => a - b).join('\n')
+  if (!effect) throw '*Lista de Efectos*\n\nEJEMPLO:\n*.textpro 1917 zabot|zabot*\n\n' + effects.sort((a, b) => a - b).join('\n')
   effect = effect.toLowerCase()
-  if (!effect in effects) throw `Efek *${effect}* tidak ditemukan`
+  if (!effect in effects) throw `Efecto *${effect}* extraviado`
   let [text, ...text2] = txt.replace(effect, '').trimStart().split(split)
   text2 = text2.join(split)
   let url = global.API('xteam', '/textpro/' + effect, { text, text2 }, 'APIKEY')
